1fe52d635de504e428be3739adeb8dab44f10902
Brenton Bills <Brenton.Bills@gmail.com>
2018-02-12 10:43:09 +1000

    Wasn't displaying the full text properly. Adding in a space

--- ng-text-truncate.js
+++ ng-text-truncate.js
@@ -1,128 +1,125 @@
 (function() {
     'use strict';
 
-
     angular.module('ngTextTruncate', [])
+        .directive("ngTextTruncate", ["$compile", "ValidationServices", "CharBasedTruncation", "WordBasedTruncation",
+            function($compile, ValidationServices, CharBasedTruncation, WordBasedTruncation) {
+                return {
+                    restrict: "A",
+                    scope: {
+                        text: "=ngTextTruncate",
+                        charsThreshold: "@ngTtCharsThreshold",
+                        wordsThreshold: "@ngTtWordsThreshold",
+                        customMoreLabel: "@ngTtMoreLabel",
+                        customLessLabel: "@ngTtLessLabel",
+                        trailingSpace: "@ngTtTrailingSpace"
+                    },
+                    controller: function($scope, $element, $attrs) {
+                        $scope.toggleShow = function() {
+                            $scope.open = !$scope.open;
+                        };
 
+                        $scope.useToggling = $attrs.ngTtNoToggling === undefined;
+                        $scope.trailingSpace = Boolean($scope.trailingSpace);
+                    },
+                    link: function($scope, $element, $attrs) {
+                        $scope.open = false;
 
-    .directive("ngTextTruncate", ["$compile", "ValidationServices", "CharBasedTruncation", "WordBasedTruncation",
-        function($compile, ValidationServices, CharBasedTruncation, WordBasedTruncation) {
-            return {
-                restrict: "A",
-                scope: {
-                    text: "=ngTextTruncate",
-                    charsThreshould: "@ngTtCharsThreshold",
-                    wordsThreshould: "@ngTtWordsThreshold",
-                    customMoreLabel: "@ngTtMoreLabel",
-                    customLessLabel: "@ngTtLessLabel"
-                },
-                controller: function($scope, $element, $attrs) {
-                    $scope.toggleShow = function() {
-                        $scope.open = !$scope.open;
-                    };
+                        ValidationServices.failIfWrongThresholdConfig($scope.charsThreshold, $scope.wordsThreshold);
 
-                    $scope.useToggling = $attrs.ngTtNoToggling === undefined;
-                },
-                link: function($scope, $element, $attrs) {
-                    $scope.open = false;
+                        var CHARS_THRESHOLD = parseInt($scope.charsThreshold);
+                        var WORDS_THRESHOLD = parseInt($scope.wordsThreshold);
 
-                    ValidationServices.failIfWrongThreshouldConfig($scope.charsThreshould, $scope.wordsThreshould);
+                        $scope.$watch("text", function() {
+                            $element.empty();
 
-                    var CHARS_THRESHOLD = parseInt($scope.charsThreshould);
-                    var WORDS_THRESHOLD = parseInt($scope.wordsThreshould);
+                            if (CHARS_THRESHOLD) {
+                                if ($scope.text && CharBasedTruncation.truncationApplies($scope, CHARS_THRESHOLD)) {
+                                    CharBasedTruncation.applyTruncation(CHARS_THRESHOLD, $scope, $element);
 
-                    $scope.$watch("text", function() {
-                        $element.empty();
-
-                        if (CHARS_THRESHOLD) {
-                            if ($scope.text && CharBasedTruncation.truncationApplies($scope, CHARS_THRESHOLD)) {
-                                CharBasedTruncation.applyTruncation(CHARS_THRESHOLD, $scope, $element);
-
-                            } else {
-                                $element.append($scope.text);
-                            }
-
-                        } else {
-
-                            if ($scope.text && WordBasedTruncation.truncationApplies($scope, WORDS_THRESHOLD)) {
-                                WordBasedTruncation.applyTruncation(WORDS_THRESHOLD, $scope, $element);
+                                } else {
+                                    $element.append($scope.text);
+                                }
 
                             } else {
-                                $element.append($scope.text);
+
+                                if ($scope.text && WordBasedTruncation.truncationApplies($scope, WORDS_THRESHOLD)) {
+                                    WordBasedTruncation.applyTruncation(WORDS_THRESHOLD, $scope, $element);
+
+                                } else {
+                                    $element.append($scope.text);
+                                }
+
                             }
-
-                        }
-                    });
-                }
-            };
-        }
-    ])
-
-
+                        });
+                    }
+                };
+            }
+        ])
 
     .factory("ValidationServices", function() {
         return {
-            failIfWrongThreshouldConfig: function(firstThreshould, secondThreshould) {
-                if ((!firstThreshould && !secondThreshould) || (firstThreshould && secondThreshould)) {
-                    throw "You must specify one, and only one, type of threshould (chars or words)";
+            failIfWrongThresholdConfig: function(firstThreshold, secondThreshold) {
+                if ((!firstThreshold && !secondThreshold) || (firstThreshold && secondThreshold)) {
+                    throw "You must specify one, and only one, type of threshold (chars or words)";
                 }
             }
         };
     })
 
-
-
     .factory("CharBasedTruncation", ["$compile", function($compile) {
         return {
-            truncationApplies: function($scope, threshould) {
-                return $scope.text.length > threshould;
+            truncationApplies: function($scope, threshold) {
+                return $scope.text.length > threshold;
             },
 
-            applyTruncation: function(threshould, $scope, $element) {
+            applyTruncation: function(threshold, $scope, $element) {
                 if ($scope.useToggling) {
-                    var el = angular.element(
+                    var el;
+
+                    el = angular.element(
                         "<span " +
                         "class='btn-link ngTruncateToggleText' " +
                         "ng-click='toggleShow()' " +
                         "ng-show='!open'> " +
-                        $scope.text.substr(0, threshould) +
+                        $scope.text.substr(0, threshold) +
                         "<span ng-show='!open'>...</span>");
 
                     $compile(el)($scope);
                     $element.append(el);
+                    var trailingSpace = $scope.trailingSpace ? '&nbsp;' : '';
 
-                    var el = angular.element(
+                    el = angular.element(
                         "<span " +
                         "class='btn-link ngTruncateToggleText' " +
                         "ng-click='toggleShow()' " +
                         "ng-show='open'> " +
-                        $scope.text.substring(threshould) +
+                        $scope.text +
+                        trailingSpace +
                         "</span>");
 
                     $compile(el)($scope);
                     $element.append(el);
 
                 } else {
-                    $element.append($scope.text.substr(0, threshould) + "...");
+                    $element.append($scope.text.substr(0, threshold) + "...");
 
                 }
             }
         };
     }])
 
-
-
     .factory("WordBasedTruncation", ["$compile", function($compile) {
         return {
-            truncationApplies: function($scope, threshould) {
-                return $scope.text.split(" ").length > threshould;
+            truncationApplies: function($scope, threshold) {
+                return $scope.text.split(" ").length > threshold;
             },
 
-            applyTruncation: function(threshould, $scope, $element) {
+            applyTruncation: function(threshold, $scope, $element) {
                 var splitText = $scope.text.split(" ");
                 if ($scope.useToggling) {
                     var el = angular.element("<span>" +
-                        splitText.slice(0, threshould).join(" ") + " " +
+                        splitText.slice(0, threshold).join(" ") + " " +
                         "<span ng-show='!open'>...</span>" +
                         "<span class='btn-link ngTruncateToggleText' " +
                         "ng-click='toggleShow()'" +
@@ -130,7 +127,7 @@
                         " " + ($scope.customMoreLabel ? $scope.customMoreLabel : "More") +
                         "</span>" +
                         "<span ng-show='open'>" +
-                        splitText.slice(threshould, splitText.length).join(" ") +
+                        splitText.slice(threshold, splitText.length).join(" ") +
                         "<span class='btn-link ngTruncateToggleText'" +
                         "ng-click='toggleShow()'>" +
                         " " + ($scope.customLessLabel ? $scope.customLessLabel : "Less") +
@@ -141,7 +138,7 @@
                     $element.append(el);
 
                 } else {
-                    $element.append(splitText.slice(0, threshould).join(" ") + "...");
+                    $element.append(splitText.slice(0, threshold).join(" ") + "...");
                 }
             }
         };
